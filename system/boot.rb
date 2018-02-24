43dcb5166aee7b1c49f7d8bf4a46809bd3ba2414
r-glebov <r.gleboff@gmail.com>
2018-02-24 13:51:51 +0100

    initial commit

---
+++ system/boot.rb
@@ -0,0 +1,18 @@
+require "bundler/setup"
+
+begin
+  require "pry-byebug"
+rescue LoadError
+end
+
+require_relative "todo/container"
+
+Todo::Container.finalize!
+
+# Load sub-apps
+app_paths = Pathname(__FILE__).dirname.join("../apps").realpath.join("*")
+Dir[app_paths].each do |f|
+  require "#{f}/system/boot"
+end
+
+require "todo/web"
