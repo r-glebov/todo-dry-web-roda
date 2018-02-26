dfa84e4c1858a4ba84310e4c3772e284ca8947e0
r-glebov <r.gleboff@gmail.com>
2018-02-26 22:02:26 +0100

    Added Tasks index page

--- spec/support/web/helpers.rb
+++ spec/support/web/helpers.rb
@@ -1,9 +1,18 @@
+# frozen_string_literal: true
+
 module Test
   module WebHelpers
     module_function
 
     def app
       Todo::Web.app
+    end
+
+    def login(user = Factory[:user])
+      visit '/login'
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: 'secret'
+      click_on 'Log in'
     end
   end
 end
