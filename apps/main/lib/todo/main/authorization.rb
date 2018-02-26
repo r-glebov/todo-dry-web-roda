720098b26ecbe0aa15293b1d6552dca553d98b6b
r-glebov <r.gleboff@gmail.com>
2018-02-26 21:06:25 +0100

    Authorization

---
+++ apps/main/lib/todo/main/authorization.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Todo
+  module Main
+    class Authorization
+      def call(user, request)
+        name = Inflecto.camelize(
+          request.path.split('/').reject(&:empty?).first
+        )
+        Object.const_get("Todo::Main::Authorizations::#{name}").new.(user, request)
+      rescue NameError
+        false
+      end
+    end
+  end
+end
--- apps/main/system/todo/main/web.rb
+++ apps/main/system/todo/main/web.rb
@@ -4,14 +4,17 @@ require_relative "container"
 module Todo
   module Main
     class Web < Dry::Web::Roda::Application
+      setting :public_routes
+      
       configure do |config|
         config.container = Container
-        config.routes = "web/routes".freeze
+        config.routes = 'web/routes'.freeze
+        config.public_routes = %w[/ /login /logout /signup]
       end
 
       opts[:root] = Pathname(__FILE__).join("../../..").realpath.dirname
 
-      use Rack::Session::Cookie, key: "todo.main.session", secret: self["core.settings"].session_secret
+      use Rack::Session::Cookie, key: 'todo.main.session', secret: self['core.settings'].session_secret
 
       plugin :csrf, raise: true
       plugin :dry_view
@@ -20,10 +23,15 @@ module Todo
       plugin :multi_route
 
       route do |r|
+        unless authorize
+          flash[:alert] = 'Unauthorized!'
+          r.redirect '/'
+        end
+        
         r.multi_route
 
         r.root do
-          r.view "welcome"
+          r.view 'welcome'
         end
       end
 
@@ -33,7 +41,6 @@ module Todo
                               .users.by_pk(session[:user_id]).one
       end
 
-      # Request-specific options for dry-view context object
       def view_context_options
         {
           flash:        flash,
@@ -45,6 +52,17 @@ module Todo
       end
 
       load_routes!
+
+      private
+
+      def config
+        self.class.config
+      end
+
+      def authorize
+        return true if config.public_routes.include? request.path
+        self.class['authorization'].(current_user, request)
+      end
     end
   end
 end
